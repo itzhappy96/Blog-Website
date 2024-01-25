cf738b980d58fe1f498793b31ba435038377eb27
itzhappy96 <ssudiptasahu001@gmail.com>
2024-01-25 15:22:20 +0530

    Add Commit

--- article/models.py
+++ article/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title=models.CharField(max_length=100)
+
+    content=models.CharField(max_length=2000)
+
+    img=models.ImageField(upload_to='article_images/' , default='default_img.jpg')
+
+    #add  a date file
+    pub_date=models.DateField(auto_now=True)
+
+def __str__(self):
+    return self.title
